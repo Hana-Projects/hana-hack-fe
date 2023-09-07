cbdd814d385ccdd1b678a0bef01108e9c2165de6
inthhh <glass005533@gmail.com>
2023-09-08 01:47:04 +0900

    style: css

--- hana-bus/src/Five.js
+++ hana-bus/src/Five.js
@@ -11,12 +11,14 @@ function Five() {
   return (
     <div>
       <div className="mobile-screen">
-        <h1>택시 탑승 완료</h1>
+        <h1 className="text-center">택시 탑승 완료</h1>
         <img src={taxi} alt="taxi" style={{ width: "200px" }} />
         <h1>예상 소요시간</h1>
-        <h1>약 32분</h1>
+        <h1 style={{ color: "#2B9F5E", marginBottom: "0" }}>약 32분</h1>
+        <div className="greenline"></div>
         <h1>택시 번호</h1>
-        <h1>12 아 3456</h1>
+        <h1 style={{ color: "#2B9F5E", marginBottom: "0" }}>12 아 3456</h1>
+        <div className="greenline"></div>
         <div className="singo-btn">신고하기</div>
       </div>
     </div>
--- hana-bus/src/Four.js
+++ hana-bus/src/Four.js
@@ -17,14 +17,16 @@ function Four() {
         <h1 className="text-center" onClick={navigateToThree}>
           뒤로가기
         </h1>
-        <h1>
+        <h2 style={{ color: "#2B9F5E" }}>
           택시가 배정되었습니다. <br />
           택시 번호를 확인 후 탑승해주세요.
-        </h1>
+        </h2>
         <h1>택시 도착 예정 시간</h1>
-        <h1>10시 58분 (5분 뒤)</h1>
+        <h1 style={{ color: "#2B9F5E" }}>10시 58분 (5분 뒤)</h1>
         <h1>택시 번호</h1>
-        <h1 onClick={navigateToFive}>12 아 3456</h1>
+        <h1 onClick={navigateToFive} style={{ color: "#2B9F5E" }}>
+          12 아 3456
+        </h1>
         <div className="call-btn">기사에게 전화하기</div>
       </div>
     </div>
