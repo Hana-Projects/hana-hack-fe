a0f63158a5f0b0c6d712e6de4afe792274bce562
inthhh <glass005533@gmail.com>
2023-09-08 01:18:06 +0900

    feat: placesearch

--- hana-bus/src/PlaceSearch.js
+++ hana-bus/src/PlaceSearch.js
@@ -5,6 +5,7 @@ const { kakao } = window;
 const PlaceSearch = ({ searchPlace, onPlaceClick }) => {
   const [Places, setPlaces] = useState([]);
   //   console.log("Place search : ", searchPlace);
+  const [isActive, setIsActive] = useState(true);
   useEffect(() => {
     const ps = new kakao.maps.services.Places();
 
@@ -20,29 +21,32 @@ const PlaceSearch = ({ searchPlace, onPlaceClick }) => {
 
   const handleItemClick = (placeName) => {
     onPlaceClick(placeName);
+    setIsActive(false);
   };
 
   return (
     <div>
-      <div className="result-list">
-        {Places.slice(0, 3).map((item, i) => (
-          <div key={i} style={{ marginTop: "0px" }} onClick={() => handleItemClick(item.place_name)}>
-            {/* <span>{i + 1}</span> */}
-            <div>
-              <h4>{item.place_name}</h4>
-              {item.road_address_name ? (
-                <div>
-                  <span>{item.road_address_name}</span>
+      {isActive ? (
+        <div className="result-list">
+          {Places.slice(0, 3).map((item, i) => (
+            <div key={i} style={{ marginTop: "0px" }} onClick={() => handleItemClick(item.place_name)}>
+              {/* <span>{i + 1}</span> */}
+              <div>
+                <h4>{item.place_name}</h4>
+                {item.road_address_name ? (
+                  <div>
+                    <span>{item.road_address_name}</span>
+                    <span>{item.address_name}</span>
+                  </div>
+                ) : (
                   <span>{item.address_name}</span>
-                </div>
-              ) : (
-                <span>{item.address_name}</span>
-              )}
+                )}
+              </div>
+              <div className="h-line"></div>
             </div>
-            <div className="h-line"></div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
