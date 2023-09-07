f9fa40ff8c5e2e632df9fd80026eda202475c004
leesangmin <dkwkghkdlxld2@gmail.com>
2023-09-08 01:45:17 +0900

    feat: f

--- hana-bus/src/Three.js
+++ hana-bus/src/Three.js
@@ -14,10 +14,7 @@ function Second() {
   return (
     <div>
       <div className="mobile-screen">
-        <h1 className="text-center" onClick={navigateToSecond}>
-          뒤로가기
-        </h1>
-        <h1 onClick={navigateToFour}>택시 부르는 중...</h1>
+        <h1 onClick={navigateToFour} style={{ paddingTop: "150px", color: 'green' }}>택시 부르는 중...</h1>
         <img onClick={navigateToFour} style={{ width: "200px" }} src={movingTaxi} alt="Taxi GIF" />
         <div className="no-btn" onClick={navigateToSecond}>
           호출 취소
